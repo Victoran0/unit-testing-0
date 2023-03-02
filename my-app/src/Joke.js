910237f50bb2737aad37a49ed6a8d7c401f75abb
Victoran0 <victoranosunkvic@gmail.com>
2023-03-02 21:07:09 +0100

    first commit

---
+++ my-app/src/Joke.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Joke({setup, punchline}) {
+    return (
+        <div>
+           <p>
+             {setup} {punchline}ksdjnslns
+            </p>
+        </div>
+    )
+}
